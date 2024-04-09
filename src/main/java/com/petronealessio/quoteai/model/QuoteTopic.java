4da3ddc65b281f727c083d62e87b1c54a46117dc
alessio <petronealesso@gmail.com>
2024-04-09 21:55:59 +0200

    Created enum that represents different topics for generating quotes

---
+++ src/main/java/com/petronealessio/quoteai/model/QuoteTopic.java
@@ -0,0 +1,37 @@
+package com.petronealessio.quoteai.model;
+
+/**
+ * The QuoteTopic enum represents different topics for generating quotes.
+ * Each topic corresponds to a specific category of quotes.
+ */
+public enum QuoteTopic {
+    /**
+     * The default topic for generating quotes.
+     */
+    DEFAULT,
+
+    /**
+     * The motivational topic for generating motivational quotes.
+     */
+    MOTIVATIONAL,
+
+    /**
+     * The humoristic topic for generating humorous quotes.
+     */
+    HUMORISTIC,
+
+    /**
+     * The love topic for generating quotes about love.
+     */
+    LOVE,
+
+    /**
+     * The existential topic for generating existential quotes.
+     */
+    EXISTENTIAL,
+
+    /**
+     * The friendship topic for generating quotes about friendship.
+     */
+    FRIENDSHIP
+}

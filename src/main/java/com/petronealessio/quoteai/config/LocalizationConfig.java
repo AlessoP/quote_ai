1524be93e558586376a4ce400b077454cec46ae5
alessio <petronealesso@gmail.com>
2024-04-09 22:18:22 +0200

    Configure locale resolver to resolve the locale using the 'Accept-Language' HTTP header. If the header is missing, set the default locale to Italian

---
+++ src/main/java/com/petronealessio/quoteai/config/LocalizationConfig.java
@@ -0,0 +1,30 @@
+package com.petronealessio.quoteai.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
+
+/**
+ * Resolves the locale using an “Accept-Language” HTTP header retrieved from an HTTP request.
+ * If into HTTP header there isn't the key “Accept-Language”, the locale is ITALIAN
+ */
+@Configuration
+public class LocalizationConfig {
+
+    /**
+     * Configure locale resolver
+     *
+     * @return LocalResolver
+     */
+    @Bean
+    public LocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver lr = new AcceptHeaderLocaleResolver();
+        //Set default locale for this locale resolver to return in the case no Accept-Language is found
+        lr.setDefaultLocale(Locale.ITALIAN);
+        return lr;
+    }
+
+}

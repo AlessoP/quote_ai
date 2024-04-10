46b9fbc22fd49f7acf8b7e855492c53c3ab0446f
alessio <petronealesso@gmail.com>
2024-04-11 00:12:54 +0200

    Remove unused import

--- src/main/java/com/petronealessio/quoteai/config/WebConfig.java
+++ src/main/java/com/petronealessio/quoteai/config/WebConfig.java
@@ -1,7 +1,6 @@
 package com.petronealessio.quoteai.config;
 
 import com.petronealessio.quoteai.util.StringToQuoteTopicConverter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;

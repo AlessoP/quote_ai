aaae3929ccf2221b6b73b94e0c998d5c3bb491de
alessio <petronealesso@gmail.com>
2024-04-23 09:43:43 +0200

    Database configuration for managing the datasource and database connections

---
+++ src/main/java/com/petronealessio/quoteai/config/QuoteDatabaseConfig.java
@@ -0,0 +1,13 @@
+package com.petronealessio.quoteai.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * Database configuration for managing the datasource and database connections.
+ */
+@Configuration
+@EnableTransactionManagement
+public class QuoteDatabaseConfig {
+
+}

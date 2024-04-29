07837aa469640cb03b58b479f8ba081e5fbe12c7
alessio <petronealesso@gmail.com>
2024-04-29 22:50:04 +0200

    Notify the websocket client with new generated quote. Added method to handle the get request to retrieve the last 10 generated quotes.

--- src/main/java/com/petronealessio/quoteai/controller/QuoteAiController.java
+++ src/main/java/com/petronealessio/quoteai/controller/QuoteAiController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +29,9 @@ public class QuoteAiController {
     @Autowired
     private QuoteDatabaseManager quoteDatabaseManager;
 
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
     /**
      * Handles the GET request to generate a quote based on the specified topic.
      *
@@ -46,17 +50,29 @@ public class QuoteAiController {
         // Saves the quote on database
         Quote savedQuote = quoteDatabaseManager.saveQuote(quote);
 
-        // Returns generated quote
         if (savedQuote != null){
             logger.info("Quote saved with id " + savedQuote.getId());
 
-            // Returns the saved quote as ResponseEntity
-            return ResponseEntity.ok(savedQuote);
+            // Replaces the generated quote with the saved quote
+            quote = savedQuote;
         } else {
             logger.warn("The quote could not be saved. The generated quote is provided without an id");
-
-            // Returns the generated quote as ResponseEntity
-            return ResponseEntity.ok(quote);
         }
+
+        // Notify the Websocket client
+        messagingTemplate.convertAndSend("/topic/newQuote", quote);
+
+        // Returns the quote as ResponseEntity
+        return ResponseEntity.ok(quote);
+    }
+
+    /**
+     * Retrieves the last 10 generated quotes.
+     *
+     * @return ResponseEntity containing the last 10 generated quotes.
+     */
+    @GetMapping(value = "v1/last_quotes")
+    public ResponseEntity<Object> last10GeneratesQuotes(){
+        return ResponseEntity.ok(quoteDatabaseManager.last10GeneratedQuotes());
     }
 }
